757ede464b646d38478e862717a310e729ea8b34
kitsunecat <scooping.fox@gmail.com>
2018-11-22 11:58:31 +0900

    upload heroku test

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -19,10 +19,12 @@ class UserSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup user infomation" do
     get signup_path
     assert_template 'users/new'
-    assert_difference 'User.count', 0 do
+    assert_no_difference 'User.count' do
       post signup_path, params: { user: {name: '', email: '',password: '',password_confirmation: '' }}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
 end
