1fca030a3ac4a0b00046a82b3d8ee6a447c9c2e8
kitsunecat <scooping.fox@gmail.com>
2018-12-01 00:50:55 +0900

    delete model belonging and payment

--- app/models/belonging.rb
+++
@@ -1,5 +0,0 @@
-class Belonging < ApplicationRecord
-  belongs_to :plan_detail
-
-  validates :plan_detail_id, presence: true
-end
--- app/models/payment.rb
+++
@@ -1,5 +0,0 @@
-class Payment < ApplicationRecord
-  belongs_to :plan_detail
-
-  validates :plan_detail_id, presence: true
-end
