7cfb7d9d5c1b5ef4497482883178a2789edf3fc9
kitsunecat <scooping.fox@gmail.com>
2018-12-05 12:03:46 +0900

    hide same day in plan_detail show

--- app/helpers/plan_details_helper.rb
+++ app/helpers/plan_details_helper.rb
@@ -2,14 +2,21 @@
 
 module PlanDetailsHelper
 
+  # 一個前のアクティビティと日付が変わっていたら日付を描写
   def drow_date(plan_detail, format)
-    current_date = plan_detail.date.strftime(format)
-    if @remember_date
-      prev_date = @remember_date[format.to_sym] if @remember_date[:format]
-      return plan_detail.date.strftime(format) if current_date != prev_date
-      @remember_date = {format: plan_detail.date.strftime(format)}
-    else
-      prev_date = @remember_date = {format: plan_detail.date.strftime(format)}
+    plan = plan_detail.plan
+
+    # formatに応じて現在の日時、一個前の日時を取得
+    current_date = plan_detail.date.strftime('%Y') if format == 'year'
+    current_date = plan_detail.date.strftime('%m/%d') if format == 'day'
+    prev_date = plan.remember_year if format == 'year' && plan.remember_year
+    prev_date = plan.remember_day if format == 'day' && plan.remember_day
+
+    # 一個前と違う日時であれば現在の日時を返す
+    if prev_date != current_date
+      plan.remember_date(current_date, format)
+      return current_date
+    else return ''
     end
   end
 
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Plan < ApplicationRecord
+  attr_accessor :remember_year, :remember_day
+
   has_many :plan_details, dependent: :destroy
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
@@ -8,4 +10,10 @@ class Plan < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, length: { maximum: 140 }
+
+  #最後に描写した日時を記録する
+  def remember_date(date, format)
+    self.remember_year = date if format == 'year'
+    self.remember_day = date if format == 'day'
+  end
 end
