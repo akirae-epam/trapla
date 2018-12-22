483bc3c291b3f5718657d90bb8531b45a849a930
kitsunecat <scooping.fox@gmail.com>
2018-12-23 00:00:26 +0900

    refactor

--- Gemfile
+++ Gemfile
@@ -35,7 +35,6 @@ end
 group :development do
   gem 'listen', '3.1.5'
   gem 'scss_lint', require: false
-  gem 'spring',                '2.0.2'
   gem 'spring-watcher-listen', '2.0.1'
   gem 'web-console',           '3.5.1'
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,28 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# frozen_string_literal: true
+
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
 
-listen "/tmp/sockets/unicorn.sock"
-pid    "/tmp/sockets/unicorn.pid"
+listen '/tmp/sockets/unicorn.sock'
+pid    '/tmp/sockets/unicorn.pid'
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Rails.logger.debug 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    Rails.logger.debug 'Unicorn worker intercepting TERM and doing nothing.'
+    Rails.logger.debug 'Wait for master to send QUIT.'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
 
 stderr_path File.expand_path('/vagrant/trapla/log/unicorn.log', ENV['RAILS_ROOT'])
