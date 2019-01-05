316e752240b84a8526f3aedd2474285fad65d790
kitsunecat <scooping.fox@gmail.com>
2019-01-05 21:55:53 +0900

    add search

--- docker/rails/unicorn.rb
+++ docker/rails/unicorn.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: truedoc
+
 worker_processes 8
 
-pid "/var/run/unicorn.pid"
-listen "/share/unicorn.sock"
+pid '/var/run/unicorn.pid'
+listen '/share/unicorn.sock'
 
-stdout_path "./log/unicorn.stdout.log"
-stderr_path "./log/unicorn.stderr.log"
+stdout_path './log/unicorn.stdout.log'
+stderr_path './log/unicorn.stderr.log'
